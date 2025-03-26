94e01fc6875ae5a20e364497318a559d987a7b30
Enio <46166345+EnioSousa@users.noreply.github.com>
2025-03-26 18:31:18 +0000

    Create executable (#2)
    Enable bot to be ran as an executable

--- log/log.py
+++ log/log.py
@@ -2,11 +2,20 @@ import logging
 import logging.config
 import yaml
 import os
+import sys
 
 def setup_logging():
-    with open("log/logging.yaml", "r") as config_file:
+    # Determine the correct path for the logging configuration
+    if getattr(sys, 'frozen', False):
+        logging_yaml_path = os.path.join(sys._MEIPASS, 'log', 'logging.yaml')
+    else:
+        logging_yaml_path = os.path.join(os.path.dirname(__file__), 'logging.yaml')
+    
+    # Load the logging configuration
+    with open(logging_yaml_path, 'r') as config_file:
         config = yaml.safe_load(config_file)
 
+    # Remove existing log files if they exist
     for handler in config.get('handlers', {}).values():
         if 'filename' in handler:
             log_file_path = handler['filename']
@@ -14,4 +23,5 @@ def setup_logging():
                 os.remove(log_file_path)
                 print(f"Deleted existing logging file: {log_file_path}")
 
+    # Apply the logging configuration
     logging.config.dictConfig(config)
